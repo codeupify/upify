9ad51fe3614aecdf4ecb9c3e1c775a08601254bb
acidcoder1 <126134595+acidcoder1@users.noreply.github.com>
2024-11-13 14:38:18 -0700

    adding version

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = "0.96.0"
+
 var rootCmd = &cobra.Command{
-	Use:   "upify",
-	Short: "Upify helps you quickly and easily deploy apps in the cloud",
-	Long:  `Upify is a platform and cloud agnostic CLI tool designed to simplify cloud deployments`,
+	Use:     "upify",
+	Short:   "Upify helps you quickly and easily deploy apps in the cloud",
+	Long:    `Upify is a platform and cloud agnostic CLI tool designed to simplify cloud deployments`,
+	Version: version,
 }
 
 func Execute() error {
+	rootCmd.SetVersionTemplate("Upify version: {{.Version}}\n") // Customize the version output if needed
 	return rootCmd.Execute()
 }
